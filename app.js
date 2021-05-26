2206f6174f85d2ca48a8f638032288777ca3e66d
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-05-25 21:14:17 -0500

    adding loop to app

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ console.log(data);
 //write code that appends a table to your web page and then adds new rows of data for each UFO sighting
 //have a column for date/time, city, state, country, shape, and comment
 
+//1. loop through data and console.log each UFO report
+data.forEach(function(data) {
+    console.log(data)
+});
 
 
 //Use a date form in your HTML document and write JavaScript code that will listen for events and search through
