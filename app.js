82f4c127ae3d8628129164eaf0aa63f293bb9823
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-05-25 21:00:57 -0500

    pulling data to app file

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 // from data.js
 var tableData = data;
 
+//collecting the data
+console.log(data);
+
 //write code that appends a table to your web page and then adds new rows of data for each UFO sighting
 //have a column for date/time, city, state, country, shape, and comment
+
+
 
 //Use a date form in your HTML document and write JavaScript code that will listen for events and search through
 //the date/time column to find rows that match user input.
