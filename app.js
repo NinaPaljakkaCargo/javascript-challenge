48515999c25b97babca65621e8ec9e6fbba9ddeb
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-05-25 21:52:13 -0500

    troubleshooting variables

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // from data.js
-var tableData = data;
+//var tableData = data;
 var tbody = d3.select("tbody");
 
 //collecting the data
-console.log(tableData);
+console.log(UFOreport);
 
 //write code that appends a table to your web page and then adds new rows of data for each UFO sighting
 //have a column for date/time, city, state, country, shape, and comment
 
 //1. loop through data and console.log each UFO report
-data.forEach(function(tableData) {
-    console.log(tableData);
+data.forEach(function(UFOreport) {
+    console.log(UFOreport);
 });
 
 
