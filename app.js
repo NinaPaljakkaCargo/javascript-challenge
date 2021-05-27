f7d94063abec7a3b644b750783a5c47122942e79
Nina Paljakka-Cargo <ninamarica17@gmail.com>
2021-05-27 18:52:43 -0500

    update

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ data.forEach(function(tableData) {
     console.log(tableData);
     var row = tbody.append("tr");
     Object.entries(tableData).forEach(function([key, value]){
-        console.log(key, value);
+        //console.log(key, value);
         var cell = row.append("td");
         cell.text(value);
     });
